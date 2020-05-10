a60735a9ffe713f3262ab4c473a2f58394e9bd7a
octavio <lojani19920520@gmail.com>
2020-05-10 09:30:43 -0500

    creacion de proyecto

---
+++ pruebaRepositorioWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pruebaRepositorioWeb.Startup))]
+namespace pruebaRepositorioWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
